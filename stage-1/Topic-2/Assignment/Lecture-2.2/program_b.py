378b415d77d55eac7ba1c62efbe9f18faa85d392
VishalRashmika <vishal.rashmika.perera@gmail.com>
2024-02-26 08:54:07 +0530

    cleaning

--- stage-1/Topic-2/Assignment/Lecture-2.2/program_b.py
+++
@@ -1,7 +0,0 @@
-marks = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-for i in range(0,10):
-    mark = int(input("Enter marks: "))
-    marks[i] = mark
-print(f"Maximum: {max(marks)}")
-print(f"Minimum: {min(marks)}")
-print(f"Average: {sum(marks)/len(marks)}")
